fe452301995735db3afedee77d30a67193aa2fb6
Rahul <rahulvish194002@gmail.com>
2024-02-29 23:09:56 +0530

    custom middleware

--- index3.js
+++ index3.js
@@ -2,12 +2,21 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+function logger(req,res,next){
+  console.log("Request method ",req.method);
+  console.log("Request url ",req.url);
+  next();
 
+}
 app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+app.get("/submit", (req, res) => {
+  res.send("submit page");
+});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- index4.js
+++ index4.js
@@ -1,7 +1,31 @@
 import express from "express";
+import bodyParser from 'body-parser'
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { BADNAME } from "dns";
+
+var bandname ='';
+ 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+function bandNameGenerator(req,res,next){
+  bandname = req.body["street"]+req.body["pet"];
+  next();
+}
 
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bandNameGenerator);
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"/public/index.html")
+})
+
+app.post("/submit",(req,res)=>{
+res.send(`<h1> Your Band name is ${bandname}`);
+
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
